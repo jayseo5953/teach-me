7975a2758c27ffecf54c02a2488dd5be3f022a29
Jay Seo <jayseo5953@gmail.com>
2024-12-25 17:48:20 -0800

    add api layer

---
+++ src/services/api/users.js
@@ -0,0 +1,10 @@
+import api from "@/services/api/apiClient";
+
+const END_POINT = "/users";
+export async function getUsers(params) {
+  return api.get(END_POINT, params);
+}
+
+export async function getUser(userId, params) {
+  return api.get([END_POINT, userId].join("/"), params);
+}
