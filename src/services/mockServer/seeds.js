e394dcf55fa8473968f339fd4f6eb43d0624f2cc
Jay Seo <jayseo5953@gmail.com>
2024-12-31 04:34:06 -0800

    set up models and resource routes for mirage orm

--- src/services/mockServer/seeds.js
+++ src/services/mockServer/seeds.js
@@ -1,22 +1,15 @@
 const seeds = {
   users: [
     {
-      id: 1,
       firstName: 'John',
       lastName: 'Doe',
       email: 'john.doe@example.com',
     },
     {
-      id: 2,
       firstName: 'Jane',
       lastName: 'Smith',
       email: 'jane.smith@example.com',
     },
-  ],
-  movies: [
-    { title: 'Interstellar', userId: 1 },
-    { title: 'Inception', userId: 1 },
-    { title: 'Dunkirk', userId: 2 },
   ],
 };
 export default seeds;
--- src/services/mockServer/server.js
+++ src/services/mockServer/server.js
@@ -1,12 +1,32 @@
-import { belongsTo, createServer, hasMany, Model } from 'miragejs';
+import { belongsTo, createServer, hasMany, Model, Factory } from 'miragejs';
 import seeds from '@/services/mockServer/seeds';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 export default function createMockServer() {
   const server = createServer({
+    // models enables using schema and relaltionship
+    models: {
+      user: Model.extend({ movies: hasMany() }),
+      movie: Model.extend({ user: belongsTo() }),
+    },
+
+    // for creating dynamic mock content
+    factories: {
+      movie: Factory.extend({
+        // could use faker.js for contenteww
+        title: (i) => `Movi Title ${i + 1}`,
+        content: (i) => `Content for movie ${i + 1}`,
+      }),
+    },
+
+    // use server.create and assign models to another model to create relationship instead of setting ids in the seeds file to avoid typos
     seeds(server) {
-      server.db.loadData(seeds);
+      const user1 = server.create('user', seeds.users[0]);
+      const user2 = server.create('user', seeds.users[1]);
+
+      server.createList('movie', 2, { user: user1 });
+      server.createList('movie', 2, { user: user2 });
     },
 
     routes() {
@@ -15,22 +35,26 @@ export default function createMockServer() {
       // auth
       this.post('/auth/login', (/* _schema, { requestBody } */) => {
         // const { email, password } = JSON.parse(requestBody);
-        // TODO: use the provided input to authenticate
-
         return true;
       });
 
       // users
-      this.get('/users', (schema, _request) => {
-        return schema.db.users;
-      });
-      this.get('/users/:id', (schema, request) => {
-        const id = request.params.id;
-        return schema.db.users.find(id);
-      });
-      this.post('/users', (schema, request) => {
-        let attrs = JSON.parse(request.requestBody);
-        return schema.db.users.insert(attrs);
+      this.resource('users'); // shorthands for below if need to customize one (adding filters), uncomment it
+      // this.get('/users');
+      // this.get('/users/:id');
+      // this.post('/users');
+      // this.patch('/users/:id');
+      // this.put('/users/:id');
+      // this.del('/users/:id');
+
+      // movies
+      this.resource('movies');
+
+      // nested routes need to be created separately
+      this.get('/users/:userId/movies', (schema, request) => {
+        const { userId } = request.params;
+        const user = schema.users.find(userId);
+        return user ? user.movies : [];
       });
     },
   });
