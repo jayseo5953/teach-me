75fcf3a65b097b6312d1c71576b3a3865d260a6f
unknown <dkim.damian@gmail.com>
2025-01-04 22:05:47 -0800

    hotfix attempt

--- src/services/api/lectures.js
+++ src/services/api/lectures.js
@@ -1,8 +1,8 @@
 import api from '@/services/api/client';
 
 const END_POINT = '/api/lectures';
-const user = '677a08c430664fe2645d4c43';
-const student = '677a08c530664fe2645d4c46';
+const user = '677a1f33be4c699c3bc402b9';
+const student = '677a1f33be4c699c3bc402bb';
 
 export async function createLecture({ subject, topic }) {
   const response = await api.post(END_POINT, {
--- src/services/api/topics.js
+++ src/services/api/topics.js
@@ -1,8 +1,9 @@
 import api from '@/services/api/client';
 
 const END_POINT = '/api/texts/analyze-subject-text';
+const student = '677a1f33be4c699c3bc402bb';
 
 export async function generateTopics(subjectText) {
-  const response = await api.post(END_POINT, { subjectText });
+  const response = await api.post(END_POINT, { subjectText, student });
   return response.data;
 }
