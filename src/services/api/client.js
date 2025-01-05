16209ab7add520e166ba79cc0373d2d04851a6dd
Jay Seo <jayseo5953@gmail.com>
2025-01-05 06:24:25 -0800

    add progression

--- src/services/api/client.js
+++ src/services/api/client.js
@@ -3,7 +3,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 const client = axios.create({
   baseURL: API_URL,
-  timeout: 5000, // Request timeout in milliseconds
+  timeout: 10000, // Request timeout in milliseconds
   headers: {
     'Content-Type': 'application/json',
   },
